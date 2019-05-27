7e28c9b23568137806d7418bda546311815b279e
moisesbenzan <moisesbenzan@outlook.com>
2019-05-27 01:27:11 -0400

    [Version] - Version 0.01-ALPHA - Initial Git commit. - Has same functionality as python version.

---
+++ main.go
@@ -0,0 +1,274 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/akamensky/argparse"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+const VERSION = "pwnedFinder-0.01"
+const HibpBreachedApi = "https://haveibeenpwned.com/api/breachedaccount/"
+const DatabasesToday = "https://www.databases.today/search-nojs.php"
+
+// Start of ParserArgs
+
+type ParserArgs struct {
+	printVersion *bool
+	target       *[]string
+	findDumps    *bool
+	targetFile   *os.File
+}
+
+// End of ParserArgs
+
+// Start of HTTPRequest
+
+type HTTPRequest struct {
+	url        string
+	headers    map[string]string
+	parameters map[string]string
+}
+
+// End of HTTPRequest
+
+// Start of HIBPResponse
+type HIBPResponse struct {
+	Name       string
+	Title      string
+	DumpURL    string
+	IsVerified bool
+}
+
+func (data *HIBPResponse) isDownloadable() string {
+	if len(data.DumpURL) > 0 {
+		return "yes"
+	}
+	return "no"
+}
+
+func (data *HIBPResponse) isVerified() string {
+	if data.IsVerified {
+		return "yes"
+	}
+
+	return "no"
+}
+
+func (data *HIBPResponse) String() string {
+	dumpString := fmt.Sprintf("\t\tShort name: %s\n\t\tLong name: %s\n", data.Name, data.Title)
+	dumpString += fmt.Sprintf("\t\tVerified: %s\n\t\tDownloadable: %s\n", data.isVerified(), data.isDownloadable())
+
+	if len(data.DumpURL) > 0 {
+		dumpString += fmt.Sprintf("\t\tURL: %s\n", data.DumpURL)
+	}
+
+	return dumpString
+}
+
+func (data *HIBPResponse) addDumpURL(dUrl string) {
+	data.DumpURL = dUrl
+	return
+}
+
+// End of HIBPResponse
+
+// Start of PwnedTarget
+type PwnedTarget struct {
+	Credentials string
+	DumpsFound  []HIBPResponse
+}
+
+func (data *PwnedTarget) String() string {
+	if len(data.DumpsFound) < 1 {
+		return fmt.Sprintf("Report for %s: \n\tResults: Not breached.", data.Credentials)
+	}
+
+	summaryString := fmt.Sprintf("Report for %s: \n\tResults: Breached. \n\tFound %d dumps.\n", data.Credentials, len(data.DumpsFound))
+	for idx, dump := range data.DumpsFound {
+		summaryString += fmt.Sprintf("\tDump report #%d\n", idx)
+
+		summaryString = summaryString + dump.String()
+	}
+
+	return summaryString
+}
+
+//End of PwnedTarget
+
+func initParser() ParserArgs {
+	// Create new parser object
+	parser := argparse.NewParser("pwnedFinder", "Find in which dumps the Credentials (username/email) provided is pwned.")
+	args := ParserArgs{}
+
+	// Declare all the args
+	args.findDumps = parser.Flag("D", "find-dumps", &argparse.Options{Help: "Attempts to find a downloadable dump containing the target's password or hash", Default: false})
+	args.printVersion = parser.Flag("V", "version", &argparse.Options{Help: "Prints the version information and exits."})
+	args.target = parser.List("t", "target", &argparse.Options{Help: "List of targets to search for pwned Credentials."})
+	args.targetFile = parser.File("T", "target-file", os.O_RDONLY, 0444, &argparse.Options{Help: "Loads targets from a file."})
+
+	// Parse input
+	err := parser.Parse(os.Args)
+
+	if err != nil {
+		// In case of error print error and print usage
+		// This can also be done by passing -h or --help flags
+		fmt.Print(parser.Usage(err))
+	}
+
+	if *args.printVersion {
+		fmt.Println(VERSION)
+		os.Exit(0)
+	}
+
+	return args
+
+}
+
+func createHttpRequest(RequestObject *HTTPRequest) *http.Request {
+
+	req, err := http.NewRequest(http.MethodGet, RequestObject.url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Headers
+	for key, value := range RequestObject.headers {
+		req.Header.Set(key, value)
+	}
+
+	// Add http params
+	queryString := req.URL.Query()
+
+	for key, value := range RequestObject.parameters {
+		queryString.Add(key, value)
+	}
+
+	req.URL.RawQuery = queryString.Encode()
+
+	//Return request object
+	return req
+
+}
+
+func checkPwned(Target *PwnedTarget) {
+
+	client := http.Client{}
+	HttpReq := HTTPRequest{
+		url: HibpBreachedApi + url.QueryEscape(Target.Credentials),
+		headers: map[string]string{
+			"User-Agent":  VERSION,
+			"api-version": "2",
+		},
+		parameters: map[string]string{
+			"truncateResponse": "false",
+		},
+	}
+
+	req := createHttpRequest(&HttpReq)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Errored whiled doing a request.")
+		return
+	}
+
+	defer resp.Body.Close()
+	resBody, _ := ioutil.ReadAll(resp.Body)
+
+	var data []HIBPResponse
+	json.Unmarshal(resBody, &data)
+
+	Target.DumpsFound = data
+	return
+}
+
+func _searchDumps(source *HIBPResponse) {
+	client := http.Client{}
+
+	HttpReq := HTTPRequest{
+		url: DatabasesToday,
+		headers: map[string]string{
+			"User-Agent": VERSION,
+		},
+		parameters: map[string]string{
+			"for": url.QueryEscape(source.Name),
+		},
+	}
+
+	req := createHttpRequest(&HttpReq)
+
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+
+	if err != nil {
+		fmt.Println("Errored whiled doing a request.")
+		return
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tRow := doc.Find("#myTable tr").Slice(1, goquery.ToEnd).First()
+
+	dumpUrl := tRow.Find("a").AttrOr("href", "")
+	//fmt.Println(dumpUrl)
+
+	source.DumpURL = dumpUrl
+	return
+
+}
+
+func searchDumps(Target *PwnedTarget) {
+
+	for index, _ := range Target.DumpsFound {
+		_searchDumps(&Target.DumpsFound[index])
+	}
+}
+
+func loadTargetsFromFile(file *os.File) []string {
+
+	defer file.Close()
+	var targets []string
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		targets = append(targets, scanner.Text())
+	}
+
+	return targets
+}
+
+func main() {
+
+	parsedArgs := initParser()
+
+	//Finally print the collected string
+	fmt.Println(*parsedArgs.findDumps)
+	var runTargets []string
+
+	if parsedArgs.targetFile != nil {
+		runTargets = loadTargetsFromFile(parsedArgs.targetFile)
+	} else {
+		runTargets = *parsedArgs.target
+	}
+
+	for _, target := range runTargets {
+		currentTarget := PwnedTarget{target, nil}
+		checkPwned(&currentTarget)
+		if *parsedArgs.findDumps {
+			searchDumps(&currentTarget)
+		}
+		fmt.Println(currentTarget.String())
+	}
+
+}
